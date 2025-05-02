232c86237afb56f3aa967bc01d3c1e456dccdcdd
mramrohaleem <40628047+mramrohaleem@users.noreply.github.com>
2025-05-02 18:25:33 +0300

    Delete discord-music-bot-railway directory

--- discord-music-bot-railway/src/buttons/destroy.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  customId: 'destroy',
-  async execute(interaction, client) {
-    const q = client.player.getQueue(interaction.guild)
-    if (q) q.destroy()
-    client.embedMessages.delete(interaction.guild.id)
-    await interaction.update({ content:'❌ Player destroyed.', embeds:[], components:[] })
-  }
-}
