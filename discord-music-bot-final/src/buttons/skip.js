cf2b1714e8676aef958b40578a5fbdd5d5788408
mramrohaleem <40628047+mramrohaleem@users.noreply.github.com>
2025-05-02 23:05:02 +0300

    Delete discord-music-bot-final directory

--- discord-music-bot-final/src/buttons/skip.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  customId: 'skip',
-  async execute(interaction, client) {
-    const queue = client.player.getQueue(interaction.guild)
-    if (!queue) return interaction.reply({ content: '❌ No track to skip.', ephemeral: true })
-    queue.skip()
-    await interaction.update({})
-  }
-}
